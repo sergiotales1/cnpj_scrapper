7094c87837d43dc94ed4b31c002a2de52c864654
SergioTales1 <algumemailai3@gmail.com>
2024-11-10 13:41:46 -0300

    email sender added

--- scrapper/src/lib.rs
+++ scrapper/src/lib.rs
@@ -1,18 +1,22 @@
 use csv::Writer;
 use std::error::Error;
-use std::fs::File;
+use std::fs::{self, File};
 use thirtyfour::prelude::*;
 
 pub async fn run() -> Result<(), Box<dyn Error + Send + Sync>> {
-    let mut caps = DesiredCapabilities::chrome();
-    caps.set_headless()?;
-    
+    let caps = DesiredCapabilities::chrome();
     let driver = WebDriver::new("http://localhost:9515", caps).await?;
+    let url = "https://cnpj.biz/empresas/recife-pe?id=63091457";
 
-    driver.goto("https://cnpj.biz/empresas/recife-pe?id=63091457").await?;
+    driver.goto(url).await?;
 
-    let cnpj_elems = driver.find_all(By::Css("p.flex.items-center.text-sm.text-gray-500")).await?;
-    let mut wtr = Writer::from_path("cnpj.csv")?;
+    let cnpj_elems = driver
+        .find_all(By::Css("p.flex.items-center.text-sm.text-gray-500"))
+        .await?;
+
+    fs::create_dir("output")?;
+
+    let mut wtr = Writer::from_path("output/cnpj.csv")?;
 
     for cnpj_elem in cnpj_elems {
         let cnpj_value = cnpj_elem.text().await?;
@@ -30,9 +34,9 @@ pub async fn run() -> Result<(), Box<dyn Error + Send + Sync>> {
 }
 
 async fn process_emails() -> Result<(), Box<dyn Error + Send + Sync>> {
-    let file = File::open("cnpj.csv")?;
+    let file = File::open("output/cnpj.csv")?;
     let mut rdr = csv::Reader::from_reader(file);
-    let mut wtr = Writer::from_path("emails.csv")?;
+    let mut wtr = Writer::from_path("output/emails.csv")?;
 
     for result in rdr.records() {
         let record = result?;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,45 @@
-use scrapper::run;
+use dotenv::dotenv;
+use lettre::message::SinglePart;
+use lettre::transport::smtp::authentication::Credentials;
+use lettre::{SmtpTransport, Transport};
+use std::env;
 use std::error::Error;
+use std::fs::File;
+use std::io::Read;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
-    run().await?;
+    // scrapper::run().await?;
+    dotenv().ok();
+
+    let mut html_file = File::open("template/index.html")?; // Replace with your HTML file path
+    let mut html_content = String::new();
+    html_file.read_to_string(&mut html_content)?;
+
+    let personalized_content = html_content.replace("[nome da empresa]", "Sergio Tales");
+
+    let sender_mail = "sergiotalesdev@gmail.com";
+    let receiver_mail = "algumemailai3@gmail.com";
+
+    let smtp_server = "smtp.sendgrid.net";
+    let smtp_username = "apikey";
+    let smtp_password = env::var("SMTP_PASSWORD").expect("SMTP_PASSWORD not found in environment");
+
+    let email = lettre::Message::builder()
+        .from(sender_mail.parse()?)
+        .to(receiver_mail.parse()?)
+        .subject("Hello")
+        .singlepart(SinglePart::html(personalized_content))?;
+
+    let creds = Credentials::new(smtp_username.to_owned(), smtp_password.to_owned());
+
+    let mailer = SmtpTransport::relay(smtp_server)?
+        .credentials(creds)
+        .build();
+
+    match mailer.send(&email) {
+        Ok(_) => println!("Email sent successfully!"),
+        Err(e) => println!("Could not send email: {:?}", e),
+    }
     Ok(())
 }
